5f9237b99096eb9ecb0f8dea1e201a2a14093a6f
James Grieve <jgrieve@ualberta.ca>
2022-06-15 10:51:07 -0600

    Bonus question;

--- Day01_InClass.sql
+++ Day01_InClass.sql
@@ -3,4 +3,6 @@
     2. Insert 5 additional rows into products, at least one referencing the new category.
     3. Select all of your additional rows from products, no previous ones. (HINT: > operator)
     4. Select a count of all types of ice cream. (HINT: LIKE keyword)
+
+    Bonus: Select a count of each category. (HINT: GROUP BY keyword)
 */
