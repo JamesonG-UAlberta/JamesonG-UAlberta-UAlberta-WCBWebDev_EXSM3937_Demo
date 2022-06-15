9c03458a49fafdf56f24b8b7c172b695b878519e
James Grieve <jgrieve@ualberta.ca>
2022-06-15 11:02:30 -0600

    Solution for in-class;

---
+++ Day01_InClass_Solution.sql
@@ -0,0 +1,19 @@
+INSERT INTO categories (name, description)
+VALUES
+('Candy', 'All the tooth decaying goodness.');
+
+INSERT INTO products (name, qoh, price, category_id)
+VALUES
+('Sour Patch Kids', 3, 3.00, 5),
+('Chocolate Bar', 5, 1.50, 5),
+('6pk Burger Buns', 2, 1.20, 1),
+('Milk 0.5L 1%', 3, 1.20, 2),
+('Milk 0.5L 2%', 7, 1.35, 2);
+
+SELECT * 
+FROM products 
+WHERE id > 20;
+
+SELECT COUNT(*)
+FROM products 
+WHERE name LIKE 'Ice Cream%';
