377b161068e2a603afa819c8a830acce840046f5
James Grieve <jgrieve@ualberta.ca>
2022-06-17 09:38:17 -0600

    Exercise 01;

---
+++ Exercise_01.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE exercise_01;
+
+CREATE TABLE Clients (
+    ID int PRIMARY KEY AUTO_INCREMENT,
+    Name varchar(30),
+    Address varchar(40),
+    PhoneNumber char(12)
+);
+CREATE TABLE Orders (
+    Number int PRIMARY KEY AUTO_INCREMENT,
+    ClientID int REFERENCES Clients(ID),
+    OrderQty int,
+    OrderDate date
+);
+
+INSERT INTO Clients (Name, Address, PhoneNumber)
+VALUES
+('Jane Smith', '123 Main Street', '123-456-7890');
+
+INSERT INTO Orders (ClientID, OrderQty, OrderDate)
+VALUES
+(1, 11, 2023-06-02);
+
+UPDATE Orders
+SET OrderQty = 1
+WHERE Number = 1;
+
+DELETE FROM Orders
+WHERE Number = 1;
+
