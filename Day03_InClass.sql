1d17a548922e492192bf1af9093a9787caf95cf1
James Grieve <jgrieve@ualberta.ca>
2022-06-17 10:44:47 -0600

    Day 03 in class;

---
+++ Day03_InClass.sql
@@ -0,0 +1,4 @@
+/* 
+    1. Show the average conversion rate from "US Dollar" to "Yen" on July 1, 2001. (HINT: currency > currencyrate)
+    2. Show the full name of the CEO of Adventure Works (HINT: employee > contact) 
+*/
